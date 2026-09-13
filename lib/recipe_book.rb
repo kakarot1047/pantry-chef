c4ed479c95b7a517ee82cb60ee83a9861730404a
Gokulan Valavan <gokulan.valavan@tamu.edu>
2026-09-13 01:07:57 -0500

    Add recipe book lookup and duplicate protection also cleared errors with rubocop and rspec

--- lib/recipe_book.rb
+++ lib/recipe_book.rb
@@ -9,49 +9,49 @@ module PantryChef
   class RecipeBook
     def initialize(recipes = [])
       @recipes = {}
-      recipes.each {|r| add(r)}
+      recipes.each { |r| add(r) }
     end
 
-  def add(recipe)
+    def add(recipe)
       raise ValidationError, 'only Recipe objects can be added' unless recipe.is_a?(Recipe)
-      raise ValidationError, "a recipe named '#{recipe.name}' already exists" if @recipes.key?(recipe.name)
+      if @recipes.key?(recipe.name)
+        raise ValidationError,
+              "a recipe named '#{recipe.name}' already exists"
+      end
 
       @recipes[recipe.name] = recipe
     end
 
-  def find(name)
-    key = name.to_s.strip.downcase
-    return nil if key.empty?
+    def find(name)
+      key = name.to_s.strip.downcase
+      return nil if key.empty?
 
-    @recipes[key]
+      @recipes[key]
+    end
+
+    def all
+      @recipes.values.sort_by(&:name)
+    end
+
+    def size
+      @recipes.size
+    end
+
+    def empty?
+      @recipes.empty?
+    end
+
+    def to_h
+      { 'recipes' => @recipes.transform_values(&:to_h) }
+    end
+
+    def self.from_h(hash)
+      raise ValidationError, 'recipe book data must be a hash' unless hash.is_a?(Hash)
+
+      entries = hash.transform_keys(&:to_s)['recipes'] || {}
+      raise ValidationError, "'recipes' must be a hash keyed by recipe name" unless entries.is_a?(Hash)
+
+      new(entries.values.map { |h| Recipe.from_h(h) })
+    end
   end
-
-  def all
-    @recipes.values.sort_by(&:name)
-  end
-
-  def size
-    @recipes.size
-  end
-
-  def empty?
-    @recipes.empty?
-  end
-
-  def to_h
-    { 'recipes' => @recipes.transform_values(&:to_h)}
-  end
-
-  def self.from_h(hash)
-    raise ValidationError, 'recipe book data must be a hash' unless hash.is_a?(Hash)
-
-    entries = hash.transform_keys(&:to_s)['recipes'] || {}
-    raise ValidationError, "'recipes' must be a hash keyed by recipe name" unless entries.is_a?(Hash)
-    
-
-    new(entries.values.map {|h| Recipe.from_h(h)})
-  end
-end
-
-
 end
